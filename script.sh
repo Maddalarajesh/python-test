5666928a6602cc2e22da5f7414134cf3ba4281f1
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2023-02-06 15:43:14 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,11 +8,6 @@ cd Fuel-Consumption-Rating
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
-git clone https://github.com/Maddalarajesh/flight-perdiction.git
-cd flight-perdiction
-pip3 install -r requirements.txt
-screen -m -d python3 app.py
-
 git clone https://github.com/Maddalarajesh/hearing.git
 cd hearing
 pip3 install -r requirements.txt
